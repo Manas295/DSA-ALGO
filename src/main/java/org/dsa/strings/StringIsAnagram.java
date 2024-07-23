f10cbc10a33ee28b049468355f8fbdaaf06f66cc
manas295 <manasbansal672@gmail.com>
2024-07-23 23:06:10 +0530

    String Anagram Question added

---
+++ src/main/java/org/dsa/strings/StringIsAnagram.java
@@ -0,0 +1,47 @@
+package org.dsa.strings;
+
+import java.util.Arrays;
+
+public class StringIsAnagram {
+    public static void main(String[] args) {
+        String s1 = "listen";
+        String s2 = "szilent";
+        //-------O(nlogn)--------
+        char[] a1 = s1.toCharArray();
+        Arrays.sort(a1);
+        String str1 = new String(a1);
+        char[] a2 = s2.toCharArray();
+        Arrays.sort(a2);
+        String str2 = new String(a2);
+        System.out.println(str1 == str2);
+
+        boolean res = isAnagram("aabca", "acaba");
+        System.out.println("is Anagram " + res);
+
+
+    }
+
+    static boolean isAnagram(String s1, String s2) {
+
+        //APPROACH 2 - Create a character arrays of 256
+        boolean isAnagram = false;
+        final int CHAR = 256;
+        if (s1.length() != s2.length()) {
+            isAnagram = false;
+            return isAnagram;
+        }
+        int count[] = new int[CHAR];
+        for (int i = 0; i < s1.length(); i++) {
+            count[s1.charAt(i)]++;
+            count[s2.charAt(i)]--;
+        }
+        for (int i = 0; i < CHAR; i++) {
+            if (count[i] != 0) {
+                isAnagram = false;
+            } else {
+                isAnagram = true;
+            }
+        }
+        return isAnagram;
+    }
+}
