ca2b2f88323224dbe142797b9cf63b2eedab018a
manas295 <manasbansal672@gmail.com>
2024-07-23 22:18:02 +0530

    Refactoring of String question

--- src/main/java/org/dsa/strings/CheckStringIsSubsequenceOfOther.java
+++ src/main/java/org/dsa/strings/CheckStringIsSubsequenceOfOther.java
@@ -4,14 +4,16 @@ public class CheckStringIsSubsequenceOfOther {
     public static void main(String[] args) {
         String str1 = "ABCDEF";
         String str2 = "ADE";//AED
+        int n = str1.length();
+        int m = str2.length();
 
         int j=0;
-        for(int i=0; i<str1.length() && j<str2.length(); i++){
+        for(int i=0; i<n && j<m; i++){
             if(str1.charAt(i)==str2.charAt(j)){
                 j++;
             }
         }
-        if(j==str2.length()){
+        if(j==m){
             System.out.println("true");
         }else{
             System.out.println("false");
