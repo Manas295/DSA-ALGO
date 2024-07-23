fdcbf4d2cebc131f5614b4fd4e91f66157afd582
manas295 <manasbansal672@gmail.com>
2024-07-23 22:16:39 +0530

    Adding String Question

---
+++ src/main/java/org/dsa/strings/CheckStringIsSubsequenceOfOther.java
@@ -0,0 +1,20 @@
+package org.dsa.strings;
+
+public class CheckStringIsSubsequenceOfOther {
+    public static void main(String[] args) {
+        String str1 = "ABCDEF";
+        String str2 = "ADE";//AED
+
+        int j=0;
+        for(int i=0; i<str1.length() && j<str2.length(); i++){
+            if(str1.charAt(i)==str2.charAt(j)){
+                j++;
+            }
+        }
+        if(j==str2.length()){
+            System.out.println("true");
+        }else{
+            System.out.println("false");
+        }
+    }
+}
